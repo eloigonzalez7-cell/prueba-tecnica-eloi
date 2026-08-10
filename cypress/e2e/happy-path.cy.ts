b0154349ffee1bb846335a19f7db8efffac18df2
Eloi Vicenc Gonzalez Mas <eloigonzalez7@gmail.com>
2026-08-10 03:30:42 +0200

    test: add cypress happy path across three views

---
+++ cypress/e2e/happy-path.cy.ts
@@ -0,0 +1,34 @@
+describe("Podcaster happy path", () => {
+  beforeEach(() => {
+    cy.clearAllLocalStorage();
+
+    cy.intercept("GET", "**/itunes-proxy/**/toppodcasts/**", {
+      fixture: "topPodcasts.json",
+    }).as("topPodcasts");
+
+    cy.intercept("GET", "**/itunes-proxy/lookup*", {
+      fixture: "podcastLookup.json",
+    }).as("podcastLookup");
+  });
+
+  it("navigates home → podcast detail → episode detail", () => {
+    cy.visit("/");
+    cy.wait("@topPodcasts");
+
+    cy.contains("a", "The Joe Budden Podcast").click();
+    cy.wait("@podcastLookup");
+    cy.url().should("include", "/podcast/1535809341");
+    cy.get("aside").should("contain", "The Joe Budden Podcast");
+    cy.get("aside").should("contain", "The Joe Budden Network");
+    cy.contains("output", "2").should("be.visible");
+
+    cy.contains("a", "Episode 1").click();
+    cy.url().should("include", "/podcast/1535809341/episode/1000000001");
+    cy.contains("h1", "Episode 1").should("be.visible");
+    cy.get("audio").should("have.attr", "src", "https://example.com/ep1.mp3");
+    cy.contains("Hello").should("be.visible");
+
+    cy.get("aside a").contains("The Joe Budden Podcast").click();
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/podcast/1535809341`);
+  });
+});
