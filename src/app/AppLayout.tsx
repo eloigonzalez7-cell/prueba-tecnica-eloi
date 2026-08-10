2bb61a7cff3db1d505826ad5d07f5af59d009974
Eloi Vicenc Gonzalez Mas <eloigonzalez7@gmail.com>
2026-08-10 02:22:14 +0200

    feat(home): add live filter with semantic markup

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -3,10 +3,8 @@ import styles from "./AppLayout.module.css";
 
 export function AppLayout() {
   return (
-    <div className={styles.shell}>
-      <main className={styles.main}>
-        <Outlet />
-      </main>
-    </div>
+    <main className={styles.main}>
+      <Outlet />
+    </main>
   );
 }
